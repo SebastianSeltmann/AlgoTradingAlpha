8b1d1c6f372a759e03b87c5e7507da56f1636b72
Ion Tapordei <iontapordei@gmail.com>
2017-11-28 12:29:29 +0100

    Options data processing function (unfinished)

--- Strategy_Alphy.py
+++ Strategy_Alphy.py
@@ -236,6 +236,28 @@ def load_data():
 prices, prices_raw, comp_const, CRSP_const = load_data()
 
 
+def store_options():
+
+    # Run load_data function first
+
+    path_input = 'C:/OptionsData/AlgoTradingData/'
+
+    paths = {}
+    paths['options'] = []
+
+    for y in range(1996, 2000):
+        paths['options'].append(path_input + "\\rawopt_" + str(y) + "AllIndices.csv")
+
+    for file in paths['options']:
+        with open(file, 'r') as o:
+            data = pd.read_csv(o)[0:100]
+            listO = pd.merge(data[['id', 'date', 'days', 'best_bid', 'impl_volatility', 'strike_price']],
+                             CRSP_const[['PERMNO']], how='inner', left_on=['id'], right_on=['PERMNO'])
+            print(listO)
+            with open("C:\\AlgoTradingData\\all_options", 'a') as f:
+                listO.to_csv(f, header=False)
+
+
 # # Optimization Process
 def objective(
         coverage,
